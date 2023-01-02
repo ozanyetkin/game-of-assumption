329a8ea8e85fb22aa6878819270983fbeffc5652
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2023-01-02 15:08:23 +0300

    chat functionality integrated but websocket fails somehow

--- gameofassumption/gameofassumption/asgi.py
+++ gameofassumption/gameofassumption/asgi.py
@@ -8,9 +8,21 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gameofassumption.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatApp.settings')
 
-application = get_asgi_application()
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter , URLRouter
+from . import routing
+
+application = ProtocolTypeRouter(
+	{
+		"http" : get_asgi_application() ,
+		"websocket" : AuthMiddlewareStack(
+			URLRouter(
+				routing.websocket_urlpatterns
+			)
+		)
+	}
+)
