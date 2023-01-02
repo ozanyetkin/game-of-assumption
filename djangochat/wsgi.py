9c6a880633f369d8f9c2e52ab54655ebf5cf69d2
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2023-01-02 15:46:50 +0300

    problem was with the channels version

--- djangochat/wsgi.py
+++ djangochat/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for gameofassumption project.
+WSGI config for djangochat project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gameofassumption.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
 
 application = get_wsgi_application()
