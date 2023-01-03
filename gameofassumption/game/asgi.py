c6ac21ea4173f021bd0bf82defb36588b69c69a6
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2023-01-03 15:13:01 +0300

    daphne initialization resolved the websocket issue, consumer turned into async

--- gameofassumption/game/asgi.py
+++ gameofassumption/game/asgi.py
@@ -8,15 +8,28 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
+import django
 
-from channels.routing import ProtocolTypeRouter
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'game.settings')
+django.setup()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "game.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+import chat.routing
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
-        # Just HTTP for now. (We can add other protocols later.)
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+        ),
     }
 )
--- gameofassumption/game/settings.py
+++ gameofassumption/game/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
+    "chat",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "chat",
-    "daphne",
 ]
 
 MIDDLEWARE = [
@@ -124,4 +124,14 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-ASGI_APPLICATION = "mysite.asgi.application"
+# Channels
+ASGI_APPLICATION = "game.asgi.application"
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
