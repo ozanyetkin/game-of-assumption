3e8c81ab4636d5c5c51b393794114bc45fcd6340
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2023-01-03 12:38:26 +0300

    index page tested

--- gameofassumption/game/settings.py
+++ gameofassumption/game/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "chat",
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "gameofassumption.urls"
+ROOT_URLCONF = "game.urls"
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "gameofassumption.wsgi.application"
+WSGI_APPLICATION = "game.wsgi.application"
 
 
 # Database
